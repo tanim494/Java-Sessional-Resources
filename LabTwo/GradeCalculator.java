f071e577c6085b1e0b72262a9e5adb125a97f0ff
Ahsanul Karim Tanim <tanim494@gmail.com>
2025-02-21 10:53:37 +0600

    Create GradeCalculator.java

---
+++ LabTwo/GradeCalculator.java
@@ -0,0 +1,25 @@
+//Lab two - Task 1
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 80) {
+            System.out.println("Grade: A+");
+        } else if (marks >= 70) {//T494
+            System.out.println("Grade: A");
+        } else if (marks >= 60) {
+            System.out.println("Grade: B"//Tanim494);
+        } else if (marks >= 50) {
+            System.out.println("Grade: C");
+        } else if (marks >= 40) {
+            System.out.println("Grade: D");
+        } else {//T494
+            System.out.println("Grade: F");
+        }
+        sc.close(//T494);
+    }
+}
