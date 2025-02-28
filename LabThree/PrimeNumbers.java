c9d7a5fb3d4ff1e92b0a9071b48178c30833bcac
Ahsanul Karim Tanim <tanim494@gmail.com>
2025-02-28 21:27:12 +0600

    Create PrimeNumbers.java

---
+++ LabThree/PrimeNumbers.java
@@ -0,0 +1,42 @@
+//Task 2
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter the ending number: ");
+        int end = sc.nextInt();
+
+        int[] primeNumbers = new int[end - start + 1]; // Maximum possible size
+        int count = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                primeNumbers[count] = i;
+                count++;
+            }
+        }
+
+        System.out.printf("Total prime numbers between %d - %d = %d\n" , start, end, count);
+        // Printing the prime numbers
+        System.out.println("Prime numbers in the given range:");
+        for (int j = 0; j < count; j++) {
+            System.out.print(primeNumbers[j] + " ");
+        }
+        sc.close();
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num < 2) return false; // Prime numbers start from 2
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
